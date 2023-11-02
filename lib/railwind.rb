5c4a7a6de2180716f62514cc5a933643ff327fbe
Christian Bryant <christianlovescode@gmail.com>
2023-11-01 23:28:56 -0400

    bumps version

--- lib/railwind.rb
+++ lib/railwind.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "railwind/version"
+
+require_relative "railwind/generators/progress_generator"
 require_relative "railwind/generators/button_generator"
 require_relative "railwind/generators/card_generator"
 require_relative "railwind/generators/tab_generator"
 require_relative "railwind/generators/callout_generator"
-require_relative "railwind/generators/progress_generator"
 
 module Railwind
   class Error < StandardError; end
